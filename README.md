fde205ca74cc8417358359dbd78a925226806962
Jay Hineman <jay.hineman@gmail.com>
2025-09-10 19:12:43 -0400

    basic example working

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # doctest-cpp-example-no-cmake
 Bare bones example of using doctest without cmake
+
+## How to compile
+
+Make sure you have `doctest.h` available in the same directory or in your compiler's include path.
+
+To compile `factorial.cpp` using g++:
+
+```sh
+/opt/homebrew/Cellar/gcc/15.1.0/bin/g++-15 -std=c++11 factorial.cpp -o factorial.out -I./doctest/doctest
+```
+
+**Pardon the homebrew nonsense resulting from g++/gcc being aliased to clang.**
+
+Then run the tests:
+
+```sh
+./factorial
+```
---
+++ factorial.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "doctest.h"
+
+// Function to compute factorial of a positive integer
+int factorial(int n) {
+    if (n < 0) throw std::invalid_argument("Negative input not allowed");
+    int result = 1;
+    for (int i = 2; i <= n; ++i)
+        result *= i;
+    return result;
+}
+
+// Doctest unit tests
+TEST_CASE("Factorial function tests") {
+    CHECK(factorial(0) == 1);
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(5) == 120);
+    CHECK(factorial(7) == 5040);
+    CHECK_THROWS_AS(factorial(-3), std::invalid_argument);
+}
